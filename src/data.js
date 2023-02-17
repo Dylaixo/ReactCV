b142563e0c56130f1bd519aceeb43602dbcf420f
Dylaixo <marcin.log01@gmail.com>
2023-02-17 17:40:20 +0100

    pierwsze componenty

---
+++ src/data.js
@@ -0,0 +1,98 @@
+export const data =[
+    {
+        'informations': {
+            "name": "Marcin",
+            "surname": "Łogwinowicz",
+            "date": "01.03.2001r.",
+            "adress": "Poleska 21/15",
+            "city": "Białystok",
+            "phone": "793792594",
+            "email": "marcin.log01@gmail.com",
+        },
+        "about": "dasdasdasd",
+        "education": [
+            {
+                "institution": "Secondary school in Bialystok",
+                "start_year": "2017",
+                "end_year": "2020",
+                "profile": "computer science and mathematics",
+            },
+            {
+                "institution": "University of Technology in Bialystok",
+                "start_year": "2020",
+                "end_year": "now",
+                "profile": "computer science"
+            }
+                    ],
+        "experience":[
+            {
+                "name": "Driver in restaurant Halo Picka in Bialystok",
+                "start_date": "01.2021r.",
+                "end_date": "06.2021r.",
+            },
+            {
+                "name": "Driver in restaurant Pho House in Bialystok",
+                "start_date": "06.2021r.",
+                "end_date": "06.2022r.",
+            },
+            {
+                "name": "Dom Pomocy Społecznej w Białymstoku",
+                "start_date": "01.08.2022r.",
+                "end_date": "31.11.2022r.",
+                "position": "Administration Support",
+                "resposibilities": "Administrating and managment of computer network and keeping it in good technical condition, removing irregularities in the operations of computer equipment, data archiving, managing and updating website and offical BIP (Biuletynu Informacji Publicznej ) website: https://www.bip.dpsb.bialystok.pl/"
+            },
+            {
+                "name": "Północno-Wschodnia Izba Lekarsko-Weterynaryjna",
+                "start_date": "01.09.2022r.",
+                "end_date": "now",
+                "position": "Administration Support",
+                "resposibilities": "Administrating and managment of computer network and keeping it in good technical condition, removing irregularities in the operations of computer equipment, managing and updating website (https://www.izbawetbial.pl/)"
+            },
+            {
+                "name": "Spółdzielnia Inwalidów Spinwar w Białymstoku",
+                "start_date": "01.09.2022r.",
+                "end_date": "now",
+                "position": "Administration Support",
+                "resposibilities": "Administrating and managment of computer network and keeping it in good technical condition, removing irregularities in the operations of computer equipment, managing and updating website, helping employees with programs (Symfonia) and computer equipment used in company."
+            },
+        ],
+        "skills":[
+            {
+                "name": "HTML",
+                "level": "advanced"
+            },
+            {
+                "name": "CSS",
+                "level": "advanced"
+            },
+            {
+                "name": "SQL",
+                "level": "basic"
+            },
+            {
+                "name": "Python",
+                "level": "basic"
+            },
+            {
+                "name": "JavaScript",
+                "level": "basic",
+            },
+            {
+                "name": "ReactJS",
+                "level": "basic",
+            },
+            {
+                "name": "Knowledge about GitHub",
+            },
+            {
+                "name": "English",
+                "level": "B2"
+            },
+            {
+                "name": "Driver license cat. B",
+            },
+                ]
+
+    }
+]
