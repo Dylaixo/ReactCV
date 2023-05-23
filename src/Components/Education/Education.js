0646234ef1e851005b7db7dabb20b57477a9096e
Dylaixo <marcin.log01@gmail.com>
2023-05-23 14:14:41 +0200

    adding courses

--- src/Components/Education/Education.js
+++ src/Components/Education/Education.js
@@ -3,11 +3,14 @@ import { data } from '../../data';
 import './Education.css';
 import {BsFillCalendarMinusFill, BsBook } from "react-icons/bs";
 import { motion } from 'framer-motion'
-
+import {GrTechnology} from 'react-icons/gr'
+import {CgWebsite} from 'react-icons/cg'
 function Education() {
     const education = data[0].education;
+    const courses = data[0].courses
   return (
     <motion.div className='content' initial={ {opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
+      <h3 className='title'>Education</h3>
     {education.map((edu, index) => (
       <div className='contentinfo' key={index}>
         <h3>{edu.institution}</h3>
@@ -16,7 +19,15 @@ function Education() {
         <hr className='divide'></hr>
       </div>
     ))}
-
+    <h3 className='title'>Courses</h3>
+    {courses.map((course, i) =>(
+      <div className='contentinfo' key={i}>
+      <h3>{course.name}</h3>
+      <p><CgWebsite /> <a href={course.link} >Link to course</a></p>
+      <p><GrTechnology/> {course.tech}</p>
+      <hr className='divide'></hr>
+    </div>
+    ))}
   </motion.div>
   );
 }
