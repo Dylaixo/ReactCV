f3194133476b43aa32760924bfff9c8fe3a1627f
Dylaixo <marcin.log01@gmail.com>
2023-06-03 14:58:20 +0200

    public changed

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export const data =[
+export const data = [
     {
         'informations': {
             "name": "Marcin",
@@ -6,8 +6,8 @@ export const data =[
             "date": "01.03.2001r.",
             "phone": "793792594",
             "email": "marcin.log01@gmail.com",
-            "linkedin" : "https://www.linkedin.com/in/marcin-%C5%82ogwinowicz-7aa52323a/",
-            "github" : "https://github.com/Dylaixo"
+            "linkedin": "https://www.linkedin.com/in/marcin-%C5%82ogwinowicz-7aa52323a/",
+            "github": "https://github.com/Dylaixo"
         },
         "about": "I’m student of last year at the Bialystok University of Technology. In this semester I’m going to start my engineering work build in React. It will be about making IT courses to teach programming languages. I’m interested in web development. I would like to improve my skills in web development and administrating websites. I really like working in small teams and communicate to other members of team to do tasks and solve problems.",
         "education": [
@@ -23,8 +23,46 @@ export const data =[
                 "end_year": "now",
                 "profile": "Computer science"
             }
-                    ],
-        "experience":[
+        ],
+        "courses": [
+            {
+                "name": "The Complete 2023 Web Development Bootcamp",
+                "link": "https://www.udemy.com/course/the-complete-web-development-bootcamp/",
+                "tech": "HTML, CSS, Bootstrap, Javascript, JQuery, Node.js with Express, API, Git, EJS, SQL, MongoDB, React",
+            },
+            {
+                "name": "Front End Web Development Ultimate Guide",
+                "link": "https://www.udemy.com/course/learn-front-end-development/",
+                "tech": "HTML, CSS, Javascript, JQuery, Bootstrap",
+            },
+            {
+                "name": "[2023] JavaScript - Jedyny Kurs, Którego Potrzebujesz!",
+                "link": "https://www.udemy.com/course/javascript-jedyny-kurs-ktorego-potrzebujesz/",
+                "tech": "Javascript",
+            },
+            {
+                "name": "100 Days of Code: The Complete Python Pro Bootcamp for 2023",
+                "link": "https://www.udemy.com/course/100-days-of-code/",
+                "tech": "Python",
+            },
+            {
+                "name": "Learn JavaScript",
+                "link": "https://www.codecademy.com/learn/introduction-to-javascript",
+                "tech": "JavaScript",
+            },
+            {
+                "name": "Learn CSS",
+                "link": "https://www.codecademy.com/learn/learn-css",
+                "tech": "CSS",
+            },
+            {
+                "name": "Learn HTML",
+                "link": "https://www.codecademy.com/learn/learn-html",
+                "tech": "HTML",
+            },
+
+        ],
+        "experience": [
             {
                 "name": "Północno-Wschodnia Izba Lekarsko-Weterynaryjna",
                 "start_date": "01.09.2022r.",
@@ -61,51 +99,51 @@ export const data =[
                 "end_date": "06.2022r.",
             }
         ],
-        "projects":[
+        "projects": [
             {
                 "title": "Learn Programming",
                 "description": "Project that I currently doing. This is the topic of my engineering thesis - application to study programming languages. I'm resposible for Frontend and my friend for backend.",
-                "photoURL" : "https://user-images.githubusercontent.com/100869542/240060967-828c69e4-56bc-434e-b9a2-836de898edd1.png",
+                "photoURL": "https://user-images.githubusercontent.com/100869542/240060967-828c69e4-56bc-434e-b9a2-836de898edd1.png",
                 "link": "https://github.com/Dylaixo/LearnProgramming-Frontend",
                 "tech": "HTML, CSS, Redux, React, Javascript, Bootstrap, own API ( made by my friend )."
             },
             {
                 "title": "Dishes App",
                 "description": "Application which has only form build in Redux form. Also have validation and some functionalities like displaying inputs fields after check box. Deployed live.",
-                "photoURL" : "https://user-images.githubusercontent.com/100869542/238473142-01829d99-c0bb-410e-8f34-6df1277f9dbf.png",
+                "photoURL": "https://user-images.githubusercontent.com/100869542/238473142-01829d99-c0bb-410e-8f34-6df1277f9dbf.png",
                 "link": "https://zippy-lamington-71dbc7.netlify.app/",
                 "tech": "HTML, CSS, React, Typescript, Redux-form, Redux, API"
             },
             {
                 "title": "News App",
                 "description": "Application using news API. Displays news for diffrent countries",
-                "photoURL" : "https://user-images.githubusercontent.com/100869542/240025606-7a9700f7-1b7e-45d7-ba9d-3b1b639655b4.png",
+                "photoURL": "https://user-images.githubusercontent.com/100869542/240025606-7a9700f7-1b7e-45d7-ba9d-3b1b639655b4.png",
                 "link": "https://github.com/Dylaixo/NewsApp",
                 "tech": "HTML, CSS, React, JavaScript, Redux, Bootstrap, RWD, React Icons"
             },
             {
                 "title": "CV in React",
                 "description": "First React application. It display static data at website and show my technical skills. I'm updating this site and it's deployed on live server.",
-                "photoURL" : "https://user-images.githubusercontent.com/100869542/240052417-129211a0-9d67-4ab5-9028-a12b7a3af84f.png",
+                "photoURL": "https://user-images.githubusercontent.com/100869542/240052417-129211a0-9d67-4ab5-9028-a12b7a3af84f.png",
                 "link": "https://github.com/Dylaixo/CV-html-css-only",
                 "tech": "React, HTML, CSS, Bootstrap, JavaScript"
             },
             {
                 "title": "CV in HTML and CSS",
                 "description": "One of my first CV build as a website. It contains only HTML and CSS languages. First page is CV website contain informations about myself.",
-                "photoURL" : "https://user-images.githubusercontent.com/100869542/239874723-727cb38e-03c7-47df-a026-eceda3d1bde9.png",
+                "photoURL": "https://user-images.githubusercontent.com/100869542/239874723-727cb38e-03c7-47df-a026-eceda3d1bde9.png",
                 "link": "https://github.com/Dylaixo/CV-html-css-only",
                 "tech": "HTML, CSS"
             },
             {
                 "title": "Find Travel",
                 "description": "One of the university projects. I took care of frontend and know how to use tags from Django in HTML.",
-                "photoURL" : "https://user-images.githubusercontent.com/100869542/174655743-d1206cd0-e7e0-474c-8378-9cf5cf7ec6b0.png",
+                "photoURL": "https://user-images.githubusercontent.com/100869542/174655743-d1206cd0-e7e0-474c-8378-9cf5cf7ec6b0.png",
                 "link": "https://github.com/Dylaixo/DjangoProjekt",
                 "tech": "HTML, CSS, Django"
             },
-                ],
-        "skills":[
+        ],
+        "skills": [
             {
                 "name": "HTML",
                 "level": "advanced",
@@ -174,7 +212,7 @@ export const data =[
                 "name": "Driver license cat. B",
                 "description": "Driver licence since 30/01/2020."
             },
-                ]
+        ]
 
     }
 ]
